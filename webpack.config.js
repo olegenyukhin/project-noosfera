fe7473c84ae67d924df7a38ad393bd5e049dbce4
Dennis Neposeda <den.neposeda@gmail.com>
2020-09-07 15:13:21 +0400

    FEST-32 Секция карта на главной странице.

--- webpack.config.js
+++ webpack.config.js
@@ -10,55 +10,56 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      compress: true,
-      port: 9000
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
     },
     module: {
         rules: [
-          {
-            test: /\.js$/i,
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          },
-          {
-            test: /\.(s)?css$/i,
-            exclude: /node_modules/,
-            use: [
-              MiniCssExtractPlugin.loader,
-              'css-loader',
-              'sass-loader'
-            ]
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            loader: 'file-loader',
-            options: {
-              name: 'fonts/[name].[ext]'
-            }
-          },
-          {
-            test: /\.(svg|png|jp(e)?g|gif)$/i,
-            loader: 'file-loader',
-            options: {
-              name: 'img/[name].[ext]'
-            }
-        },
-        {
-          test: /\.html$/i,
-          loader: 'html-loader',
-          options: {
-            minimize: true
-          }
-        },
-      ]
+            {
+                test: /\.js$/i,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+            {
+                test: /\.(s)?css$/i,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(svg|png|jp(e)?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: 'img/[name].[ext]'
+                }
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    minimize: true
+                }
+            },
+        ]
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HTMLWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            favicon: './src/favicon.png'
         }),
         new MiniCssExtractPlugin({
             filename: 'style.css'
