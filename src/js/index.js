ad6622f3962ddce8bb69f23148c1014b095c1b77
LAPTOP-K7S31IAB\thank <thankee@yandex.ru>
2020-09-17 14:45:18 +0400

    Фикс слайдера, добавление видео-плеера

--- src/js/index.js
+++ src/js/index.js
@@ -18,7 +18,7 @@ prevSliderButton.addEventListener('click', () => {
         position = -maxPosition - 1;
     }
     position++;
-    
+
     sliderDots[-position + 1].checked = true;
     slider.style.transform = `translateX(${position * itemWidth}px)`;
 })
@@ -28,7 +28,7 @@ nextSliderButton.addEventListener('click', () => {
         position = 1;
     }
     position--;
-    
+
     sliderDots[-position + 1].checked = true;
     slider.style.transform = `translateX(${position * itemWidth}px)`;
 })
@@ -36,11 +36,86 @@ nextSliderButton.addEventListener('click', () => {
 sliderDots.forEach(dot => {
     dot.addEventListener('click', ({ target }) => {
         const number = target.dataset.number;
-        position = maxPosition - number;
+        const middleItem = Math.ceil(itemsNumber / 2);
+        position = -(number - middleItem);
 
         if (position > 0) position = 0;
-        if (position < -maxPosition) position = -2;
+        if (position < -maxPosition) position = -maxPosition;
 
         slider.style.transform = `translateX(${position * itemWidth}px)`;
     })
 })
+
+
+
+
+function findVideos() {
+    let videos = document.querySelectorAll('.video__preview');
+
+    for (let i = 0; i < videos.length; i++) {
+        setupVideo(videos[i]);
+    }
+}
+
+function setupVideo(video) {
+    const link = video.querySelector('.video__link');
+    const media = video.querySelector('.video__poster');
+    const button = video.querySelector('.video__button');
+    const videoPlayer = document.querySelector('.video__container');
+
+    const id = parseMediaURL(media);
+
+    video.addEventListener('click', () => {
+        videoPlayer.innerHTML = '';
+        const iframe = createIframe(id);
+        const videoBackground = document.createElement('div');
+        videoBackground.classList.add('video__background');
+
+        videoPlayer.style.display = 'block';
+        document.querySelector('body').appendChild(videoBackground);
+        videoPlayer.appendChild(iframe);
+
+        const closeListener = () => {
+            videoPlayer.style.display = 'none';
+            closeVideoButton.removeEventListener('click', closeListener)
+            videoPlayer.innerHTML = '';
+            videoBackground.remove();
+        }
+
+        const closeVideoButton = document.createElement('span');
+        closeVideoButton.classList.add('close');
+        videoPlayer.appendChild(closeVideoButton);
+        closeVideoButton.addEventListener('click', closeListener);
+    });
+
+    link?.removeAttribute('href');
+}
+
+function parseMediaURL(media) {
+    const regexp = /([a-zA-Z0-9_-]+)\.jpg/i;
+    const url = media?.src;
+
+    const match = url?.match(regexp) || [];
+
+    return match[1];
+}
+
+function createIframe(id) {
+    const iframe = document.createElement('iframe');
+
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+    iframe.setAttribute('src', generateURL(id));
+    iframe.classList.add('video__player');
+
+    return iframe;
+}
+
+function generateURL(id) {
+    const query = '?rel=0&showinfo=0&autoplay=1';
+
+    return 'https://www.youtube.com/embed/' + id + query;
+}
+
+findVideos();
